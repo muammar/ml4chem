1630f57ee601d26b990d56faeb88b142cbc0aa4e
Muammar El Khatib <muammarelkhatib@gmail.com>
2019-02-20 15:14:13 -0800

    This should get rid of ase import errors.
    I will just test if this works for rtd. If it does, then I will slowly
    add more and more modules to be mocked instead of pollute the history
    with useless commits.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
-MOCK_MODULES = ['ase']
+MOCK_MODULES = ['ase', 'ase.calculators']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
