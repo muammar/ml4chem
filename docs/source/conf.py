747924cdf7f851c04eb131cf9df8c9485aa85495
Muammar El Khatib <muammarelkhatib@gmail.com>
2019-02-20 15:04:43 -0800

    This possible avoids problems with rtd.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,6 +16,14 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../../'))
 
+class Mock(MagicMock):
+    @classmethod
+    def __getattr__(cls, name):
+        return MagicMock()
+
+MOCK_MODULES = ['ase']
+sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+
 
 # -- Project information -----------------------------------------------------
 
