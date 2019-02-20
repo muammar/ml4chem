ce91fe40ba7c8d85ca4dec271a600f3a606946d4
Muammar El Khatib <muammarelkhatib@gmail.com>
2019-02-20 15:05:46 -0800

    Fix NameError.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../../'))
 
-class Mock(MagicMock):
+class Mock:
     @classmethod
     def __getattr__(cls, name):
         return MagicMock()
