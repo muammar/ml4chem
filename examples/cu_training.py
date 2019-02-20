586beb92839cfbdf1075eaa9f3d4c10e15a20c9f
Muammar El Khatib <muammarelkhatib@gmail.com>
2019-02-20 15:25:09 -0800

    Added example with copper bulk.

---
+++ examples/cu_training.py
@@ -0,0 +1,33 @@
+import sys
+sys.path.append('../')
+from ase.io import Trajectory
+from mlchem import Potentials
+from mlchem.fingerprints import Gaussian
+from mlchem.models.neuralnetwork import NeuralNetwork
+
+# Load the images with ASE
+images = Trajectory('cu_training.traj')
+
+# Arguments for fingerprinting the images
+scaler = 'minmax'
+normalized = True
+
+# Arguments for building the model
+n = 10
+activation ='relu'
+
+# Arguments for training the potential
+convergence = {'energy': 5e-3}
+epochs = 100
+lr = 1e-4
+weight_decay = 0.
+regularization = 0.
+
+calc = Potentials(fingerprints=Gaussian(cutoff=6.5, normalized=normalized,
+                                        scaler=scaler),
+                  model=NeuralNetwork(hiddenlayers=(n, n),
+                                      activation=activation))
+
+calc.train(training_set=images, epochs=epochs, lr=lr,
+           weight_decay=weight_decay, regularization=regularization,
+           convergence=convergence)
