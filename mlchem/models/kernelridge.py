72ac349afd6b207dbb4b786e15670626232dcaaf
Muammar El Khatib <muammarelkhatib@gmail.com>
2019-03-14 22:12:19 -0700

    Starting kernel ridge regression implementation.

---
+++ mlchem/models/kernelridge.py
@@ -0,0 +1,225 @@
+import time
+import datetime
+
+
+class KernelRidge(object):
+    """Kernel Ridge Regression
+
+    Parameters
+    ----------
+    sigma : float, list, or dict
+        Length scale of the Gaussian in the case of RBF, exponential, and
+        laplacian kernels. Default is 1. (float) and it computes isotropic
+        kernels. Pass a list if you would like to compute anisotropic kernels,
+        or a dictionary if you want sigmas for each model.
+
+        Example:
+
+            >>> sigma={'energy': {'H': value, 'O': value},
+                       'forces': {'H': {0: value, 1: value, 2: value},
+                              'O': {0: value, 1: value, 2: value}}}
+
+        `value` can be a float or a list.
+    kernel : str
+        Choose the kernel. Available kernels are: 'linear', 'rbf', 'laplacian',
+        and 'exponential'. Default is 'rbf'.
+    lamda : float, or dictionary
+        Strength of the regularization. If you pass a dictionary then force and
+        energy will have different regularization:
+
+            >>> lamda = {'energy': value, 'forces': value}
+
+        Dictionaries are only used when performing Cholesky factorization.
+    weights : dict
+        Dictionary of weights.
+    regressor : object
+        Regressor class to be used.
+    mode : str
+        Atom- or image-centered mode.
+    trainingimages : str
+        Path to Trajectory file containing the images in the training set. This
+        is useful for predicting new structures.
+    version : str
+        Version.
+    fortran : bool
+        Use fortran code.
+    checkpoints : int
+        Frequency with which to save parameter checkpoints upon training. E.g.,
+        100 saves a checkpoint on each 100th training set.  Specify None for
+        no checkpoints. Default is None.
+    lossfunction : object
+        Loss function object.
+    cholesky : bool
+        Whether or not we are using Cholesky decomposition to determine the
+        weights. This method returns an unique set of regression coefficients.
+    weights_independent : bool
+        Whether or not the weights are going to be split for energy and forces.
+    randomize_weights : bool
+        If set to True, weights are randomly started when minimizing the L2
+        loss function.
+    forcetraining : bool
+        Turn force training true.
+    nnpartition : str
+        Use per-atom energy partition from a neural network calculator.
+        You have to set the path to .amp file. Useful for energy training with
+        Cholesky factorization. Default is set to None.
+    preprocessing : bool
+        Preprocess training data.
+    sum_rule : bool
+        Whether or not we sum of fingerprintprime elements over a given axis.
+        This applies np.sum(fingerprint_list, axis=0).
+
+    Notes
+    -----
+        In the case of training total energies, we need to apply either an
+        atomic decomposition Ansatz (ADA) during training or an energy
+        partition scheme to the training set. ADA can be achieved based on
+        Int. J.  Quantum Chem., vol. 115, no.  16, pp.  1051-1057, Aug. 2015".
+        For an explanation of what they do, see the Master thesis by Sonja
+        Mathias.
+
+        http://wissrech.ins.uni-bonn.de/teaching/master/masterthesis_mathias_revised.pdf
+
+        ADA is the default way of training total energies in this KernelRidge
+        class.
+
+        An energy partition scheme for  total energies can be obtained from an
+        artificial neural network or methods such as the interacting quantum
+        atoms theory (IQA). I implemented the nnpartition mode for which users
+        can provide the path to a NN calculator and we take the energies
+        per-atom from the function .calculate_atomic_energy(). The strategy
+        would be to use train the NN with a very tight convergence criterion
+        (1e-6 RSME).  Then, calling .calculate_atomic_energy() would give you
+        the atomic energies for such set.
+
+        For forces is a different history because we do know the derivative of
+        the energy with respect to atom positions (a per-atom quantity).  So we
+        rely on the method in the algorithm shown in Rupp, M. (2015).  Machine
+        learning for quantum mechanics in a nutshell. International Journal of
+        Quantum Chemistry, 115(16), 1058-1073.
+
+    Parameters
+    ----------
+    hiddenlayers : tuple
+        Structure of hidden layers in the neural network.
+    activation : str
+        The activation function.
+    """
+
+    NAME = 'KernelRidge'
+
+    @classmethod
+    def name(cls):
+        """Returns name of class"""
+
+        return cls.NAME
+
+    def __init__(self, sigma=1., kernel='rbf', lamda=1e-5, weights=None,
+                 regressor=None, mode=None, trainingimages=None, version=None,
+                 fortran=False, checkpoints=None, lossfunction=None,
+                 cholesky=True, weights_independent=True,
+                 randomize_weights=False, forcetraining=False,
+                 preprocessing=False, nnpartition=None, sum_rule=True):
+
+        np.set_printoptions(precision=30, threshold=999999999)
+
+    def prepare_model(self, input_dimension, data=None, purpose='training'):
+        """Prepare the model
+
+        Parameters
+        ----------
+        """
+        pass
+
+    def train(inputs, targets, model=None, data=None, optimizer=None, lr=None,
+              weight_decay=None, regularization=None, epochs=100, convergence=None,
+              lossfxn=None):
+        """Train the model
+
+        Parameters
+        ----------
+        inputs : dict
+            Dictionary with hashed feature space.
+        epochs : int
+            Number of full training cycles.
+        targets : list
+            The expected values that the model has to learn aka y.
+        model : object
+            The NeuralNetwork class.
+        data : object
+            DataSet object created from the handler.
+        lr : float
+            Learning rate.
+        weight_decay : float
+            Weight decay passed to the optimizer. Default is 0.
+        regularization : float
+            This is the L2 regularization. It is not the same as weight decay.
+        convergence : dict
+            Instead of using epochs, users can set a convergence criterion.
+        lossfxn : obj
+            A loss function object.
+        """
+        pass
+
+        """
+        #old_state_dict = {}
+
+        #for key in model.state_dict():
+        #    old_state_dict[key] = model.state_dict()[key].clone()
+
+        targets = torch.tensor(targets, requires_grad=False)
+
+        # Define optimizer
+        if optimizer is None:
+            optimizer = torch.optim.Adam(model.parameters(), lr=lr,
+                                         weight_decay=weight_decay)
+
+        print()
+        print('{:6s} {:19s} {:8s}'.format('Epoch', 'Time Stamp', 'Loss'))
+        print('{:6s} {:19s} {:8s}'.format('------',
+                                          '-------------------', '---------'))
+        initial_time = time.time()
+
+        _loss = []
+        _rmse = []
+        epoch = 0
+
+        while True:
+            epoch += 1
+
+            outputs = model(inputs)
+
+            if lossfxn is None:
+                loss, rmse = loss_function(outputs, targets, optimizer, data)
+            else:
+                raise('I do not know what to do')
+
+            _loss.append(loss)
+            _rmse.append(rmse)
+
+            ts = time.time()
+            ts = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d '
+                                                              '%H:%M:%S')
+            print('{:6d} {} {:8e} {:8f}' .format(epoch, ts, loss, rmse))
+
+            if convergence is None and epoch == epochs:
+                break
+            elif (convergence is not None and rmse < convergence['energy']):
+                break
+
+        training_time = time.time() - initial_time
+
+        print('Training the model took {}...' .format(training_time))
+        print('outputs')
+        print(outputs)
+        print('targets')
+        print(targets)
+
+        import matplotlib.pyplot as plt
+        plt.plot(list(range(epoch)), _loss, label='loss')
+        plt.plot(list(range(epoch)), _rmse, label='rmse/atom')
+        plt.legend(loc='upper left')
+        plt.show()
+
+        parity(outputs.detach().numpy(), targets.detach().numpy())
+        """
