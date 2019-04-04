b8828c1ffaca2133291557d7e49fc71c083a535c
Muammar El Khatib <muammarelkhatib@gmail.com>
2019-04-04 11:42:33 -0700

    Read log function and improved parity plot.

--- mlchem/data/visualization.py
+++ mlchem/data/visualization.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 
 
-def parity(predictions, true, filename=None, **kwargs):
+def parity(predictions, true, scores=False, filename=None, **kwargs):
     """A parity plot function
 
     Parameters
@@ -10,6 +12,8 @@ def parity(predictions, true, filename=None, **kwargs):
         Model predictions in a list.
     true : list or numpy.array
         Targets or true values.
+    scores : bool
+        Print scores in parity plot.
     filename : str
         A name to save the plot to a file. If filename is non exisntent, we
         call plt.show().
@@ -28,7 +32,52 @@ def parity(predictions, true, filename=None, **kwargs):
     plt.xlabel('True Values')
     plt.ylabel('ML4Chem Predictions')
 
+    if scores:
+        rmse = np.sqrt(mean_squared_error(true, predictions))
+        mae = mean_absolute_error(true, predictions)
+        correlation = r2_score(true, predictions)
+        plt.text(min_val, max_val, 'R-squared = {:.2f} \n'
+                                   'RMSE = {:.2f}\n'
+                                   'MAE = {:.2f}\n'
+                                   .format(correlation, rmse, mae))
+
     if filename is None:
         plt.show()
     else:
         plt.savefig(filename, **kwargs)
+
+
+def read_log(logfile):
+    """Read the logfile
+
+    Parameters
+    ----------
+    logfile : str
+        Path to logfile.
+    """
+
+    f = open(logfile, 'r')
+
+    check = "------ ------------------- ------------ -------- ---------"
+    start = False
+    epochs = []
+    loss = []
+    rmse = []
+
+    for line in f.readlines():
+
+        if check in line:
+            start = True
+
+        if start:
+            try:
+                line = line.split()
+                epochs.append(int(line[0]))
+                loss.append(float(line[3]))
+                rmse.append(float(line[4]))
+            except:
+                pass
+
+    plt.plot(epochs, rmse, label='rmse')
+    plt.legend(loc='upper left')
+    plt.show()
