0d582666c563cc7c348d0cc6c763fd523f1f0f40
ElijahGardella <Eli@cs-it-6899047.dhcp.lbl.gov>
2019-07-29 10:30:33 -0700

    --Trainging values for Neural Network

--- examples/nn_potentials_ionic/cu_training_ionic.py
+++ examples/nn_potentials_ionic/cu_training_ionic.py
@@ -1,6 +1,6 @@
 import sys
 import torch
-from ase.io import Trajectory
+from ase.io import Trajectory, write, read
 from dask.distributed import Client, LocalCluster
 
 sys.path.append("../../")
@@ -14,7 +14,9 @@ from ml4chem.optim.handler import get_optimizer
 
 def train():
     # Load the images with ASE
-    images = Trajectory("cu_training.traj")
+    images1 = Trajectory("cu_training_save.traj")
+    images2 = Trajectory("xyz.traj")
+    images1 = Trajectory("cu_training_save.traj")
 
     # Arguments for fingerprinting the images
     normalized = True
@@ -30,27 +32,62 @@ def train():
     weight_decay = 0.0
     regularization = 0.0
 
+    charges = {"HH": 0.0, "OO": -2.0, "OHH": 0.0, "OOHH":0.0, "CuCuCuCu": 1.0, "HHOO":0.0}
+
     calc = Potentials(
         fingerprints=Gaussian(
-            cutoff=6.5,
+            cutoff=1.5,
             normalized=normalized,
             save_preprocessor="model.scaler",
             angular_type="G4",
+            overwrite=True,
         ),
-        model=NeuralNetwork(hiddenlayers=(n, n), activation=activation),
+        model=NeuralNetwork(hiddenlayers=(n, n), activation=activation,
+         charges=charges, data_type="diatomic_molecules", parameterize_alpha_hardness=True),
         label="cu_training_ionic",
-        path="",
-        ionic=True,
+        
     )
     optimizer = ("adam", {"lr": lr, "weight_decay": weight_decay})
 
     calc.train(
-        training_set=images,
+        training_set=images2,
         epochs=epochs,
         regularization=regularization,
         convergence=convergence,
         optimizer=optimizer,
     )
+
+    data_handler = DataSet(images2, purpose="training")
+    # Raw input and targets aka X, y
+    training_set, targets = data_handler.get_images(purpose="training")
+
+    latent_space = calc.model.latent_space
+    latent_space = latent_space[0]
+    temp = 0.0
+    energies = []
+    errors = []
+    counter = 0
+    average = 0.0
+    for hash in latent_space:
+        energies.append(latent_space[hash][0])
+        temp = 0.0
+        for j in range(len(latent_space[hash][0])):
+            temp += latent_space[hash][0][j]
+        temp = temp - targets[counter]
+        temp /= targets[counter]
+        temp = abs(temp)
+        temp *= 100
+        average += temp
+        errors.append(temp)
+        if temp >= 1:
+            print("OVER 1 PERCENT ERROR")
+        counter += 1
+    print("errors")
+    print(errors)
+    average /= len(latent_space)
+    print("average error: " + str(average))
+    print("printing latent_space")
+    print(latent_space)
 
 
 if __name__ == "__main__":
